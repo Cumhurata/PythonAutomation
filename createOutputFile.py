75707df6ea2f9e300e5988cb3a825fe557dcc02a
Cumhur Ata <ata.cumhur@gmail.com>
2023-04-17 01:20:24 +0300

    Updated Code & Interpreter

---
+++ createOutputFile.py
@@ -0,0 +1,7 @@
+f=open("C:\\Users\\Cumhur Ata\\PycharmProjects\\PythonAutomation\\outputfile.txt", "r+");
+f.truncate(0);
+f.write("more text aresads a sa there..adsd..");
+t=open("C:\\Users\\Cumhur Ata\\PycharmProjects\\PythonAutomation\outputfile.txt", "r");
+print(t.read);
+
+

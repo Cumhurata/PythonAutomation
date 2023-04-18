e821e02864e185a49f2187ea043224a99caa2b25
Cumhur Ata <ata.cumhur@gmail.com>
2023-04-18 16:28:55 +0300

    Updated Code & Interpreter

--- createOutputFile.py
+++ createOutputFile.py
@@ -6,7 +6,5 @@ fileName = time.strftime("%Y%m%d-%H%M%S") + ".txt"
 folder_to_save_files = folder_name
 
 f=open(fileName, 'x');
-f=open(fileName, "r+");
-f.truncate(0);
-f.write("Caglar deneneme 112421");
+f.write(contentText);
 
